6afff2b51105565ff490946f45d34933c5a76734
dymoy <148160802+dymoy@users.noreply.github.com>
2024-04-06 09:41:43 -0700

    Created queries and mutations files in client/src/utils/

---
+++ client/src/utils/queries.js
@@ -0,0 +1,24 @@
+/**
+ * @file queries.js
+ * This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
+ */
+
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me{
+        me {
+            _id
+            username
+            email
+            books {
+                authors
+                description
+                title
+                bookId
+                image
+                link
+            }
+        }
+    }
+`
